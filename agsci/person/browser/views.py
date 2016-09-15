c7b4d5ce9e4d7d286c9732d50609eff8dbe272b9
TIMOTHY RIDGE SIMKINS <trs22@psu.edu>
2016-09-15 11:14:28 -0400

    Added 'Products' to Person.

--- agsci/person/browser/views.py
+++ agsci/person/browser/views.py
@@ -1,9 +1,19 @@
 from Products.Five import BrowserView
 from zope.interface import implements, Interface
+from agsci.atlas.browser.views import AtlasStructureView
 
 class IPersonView(Interface):
     pass
     
-class PersonView(BrowserView):
-    pass
+class PersonView(AtlasStructureView):
 
+    def getPersonProducts(self):
+    
+        return self.portal_catalog.searchResults({'object_provides' : 'agsci.atlas.content.IAtlasProduct',
+                                                  'Owners' : self.context.username,
+                                                  'sort_on' : 'sortable_title',
+                                                })        
+
+    @property
+    def getTileColumns(self):
+        return '4'
