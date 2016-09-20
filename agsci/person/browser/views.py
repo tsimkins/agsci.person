8ddc3d9f21ba32915616fba663fac22649e140b1
TIMOTHY RIDGE SIMKINS <trs22@psu.edu>
2016-09-20 13:29:29 -0400

    Added a view for the directory.

--- agsci/person/browser/views.py
+++ agsci/person/browser/views.py
@@ -17,3 +17,16 @@ class PersonView(AtlasStructureView):
     @property
     def getTileColumns(self):
         return '4'
+
+class DirectoryView(AtlasStructureView):
+
+    @property
+    def getTileColumns(self):
+        return '5'
+
+    @property
+    def show_image(self):
+        return False
+
+    def getPeople(self, contentFilter={}):
+        return self.portal_catalog.searchResults({'Type' : 'Person', 'sort_on' : 'sortable_title'})
