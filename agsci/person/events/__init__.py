9c80f632ac8282cbaca1f7c2b98d177c808a7834
TIMOTHY RIDGE SIMKINS <trs22@psu.edu>
2017-04-04 12:15:42 -0400

    On person edit, assign the person's username as the owner.  This allows people to edit their own profiles.

--- agsci/person/events/__init__.py
+++ agsci/person/events/__init__.py
@@ -1,16 +1,20 @@
 # Update person when person edited
+from agsci.atlas.events import assignOwnerPermission
 
 def onPersonEdit(context, event):
 
     username = getattr(context, 'username', '')
-    
+
     if username:
-    
+
         username = username.encode('utf-8')
-    
+
         if username != context.getId():
-        
+
             parent = context.aq_parent
-            
+
             if username not in parent.objectIds():
                 parent.manage_renameObjects(ids=[context.getId()], new_ids=[username])
+
+        # Assign the person's username as the owner
+        assignOwnerPermission(context, event)
