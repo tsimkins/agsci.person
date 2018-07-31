c82dbbdd5ecfe39827dfe464d67c6224e2f2fb9e
TIMOTHY RIDGE SIMKINS <trs22@psu.edu>
2018-07-31 14:14:06 -0400

    * Tweaked the Classifications vocabulary  * Added a BOM and CRM vocabulary.

--- agsci/person/content/vocabulary.py
+++ agsci/person/content/vocabulary.py
@@ -2,28 +2,30 @@ from zope.schema.interfaces import IVocabularyFactory
 from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 from zope.interface import implements
 
+from agsci.atlas.utilities import SitePeople
+
 # Directory classifications for people
 class ClassificationsVocabulary(object):
 
     implements(IVocabularyFactory)
 
+    items = [
+        'Faculty',
+        'Educator',
+        'Staff',
+        'Director',
+        'Associate Director',
+        'Assistant Director of Programs',
+        'Client Relations Manager',
+        'Business Operations Manager',
+        'Leadership Team',
+        'Volunteer',
+    ]
+
     def __call__(self, context):
 
-        classifications = [
-            'Faculty',
-            'Educator',
-            'Staff',
-            'Director',
-            'Associate Director',
-            'Assistant Director of Programs',
-            'Client Relations Manager',
-            'Business Operations Manager',
-            'Leadership Team',
-            'Volunteer',
-        ]
-
         return SimpleVocabulary(
-            [SimpleTerm(x ,title=x) for x in classifications]
+            [SimpleTerm(x ,title=x) for x in self.items]
         )
 
 # Project / Program Team
@@ -124,6 +126,40 @@ class HomeBudgetVocabulary(object):
             [SimpleTerm(x ,title=x) for x in items]
         )
 
+class PersonClassificationsVocabulary(object):
+
+    implements(IVocabularyFactory)
+
+    find_classifications = []
+
+    def __call__(self, context):
+
+        sp = SitePeople()
+
+        people = sp.getValidPeople()
+
+        filtered_people = [
+            x.getObject() for x in people if set(x.Classifications) & set(self.find_classifications)
+        ]
+
+        return SimpleVocabulary(
+            [SimpleTerm(x.username ,title=x.Title()) for x in filtered_people]
+        )
+
+class CRMVocabulary(PersonClassificationsVocabulary):
+
+    find_classifications = [
+        'Client Relations Manager',
+    ]
+
+class BOMVocabulary(PersonClassificationsVocabulary):
+
+    find_classifications = [
+        'Business Operations Manager',
+    ]
+
 ClassificationsVocabularyFactory = ClassificationsVocabulary()
 ProjectProgramTeamVocabularyFactory = ProjectProgramTeamVocabulary()
 HomeBudgetVocabularyFactory = HomeBudgetVocabulary()
+CRMVocabularyFactory = CRMVocabulary()
+BOMVocabularyFactory = BOMVocabulary()
