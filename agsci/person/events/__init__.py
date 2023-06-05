53dc6a61ec4403c076097c28966ae491a6fea75e
TIMOTHY RIDGE SIMKINS <trs22@psu.edu>
2023-06-05 10:12:14 -0400

    Unicode fix

--- agsci/person/events/__init__.py
+++ agsci/person/events/__init__.py
@@ -15,8 +15,6 @@ def setPersonUsername(context, event):
 
     if username:
 
-        username = username.encode('utf-8')
-
         if username != context.getId():
 
             parent = context.aq_parent
